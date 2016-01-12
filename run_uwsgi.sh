2552522a64d67c6719ba4cb84d5e8bed6470842c
vsoch <vsochat@stanford.edu>
2016-01-12 19:03:18 +0000

    removing credentials to fix #13

--- run_uwsgi.sh
+++ run_uwsgi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python manage.py migrate
 python manage.py collectstatic --noinput
-#python manage.py shell < scripts/create_superuser.py
+python manage.py shell < scripts/create_superuser.py
 uwsgi uwsgi.ini
