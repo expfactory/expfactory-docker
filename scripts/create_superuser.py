ef23a5f12673d3faa2dede7a28e9446dc4716e5b
vsoch <vsochat@stanford.edu>
2016-02-13 00:39:34 +0000

    modified:   scripts/create_superuser.py

--- scripts/create_superuser.py
+++ scripts/create_superuser.py
@@ -1,4 +1,4 @@
 from django.contrib.auth.models import User
-from expdj.apps.users.models import User
+#from expdj.apps.users.models import User
 if len(User.objects.all())==0:
     User.objects.create_superuser(username='expfactory', password='expfactory', email='')
