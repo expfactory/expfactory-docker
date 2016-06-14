5065dce5cc641d91da7ce9ee6f6be1bf6a128b61
Ross Blair <rblair2@stanford.edu>
2016-06-14 13:34:43 -0700

    copied over old serializers from base url conf to new experiments and turk serializers files, added result list views to view all results owned by a user and all results for a given battery that are owned by a user

--- expdj/apps/turk/urls.py
+++ expdj/apps/turk/urls.py
@@ -4,6 +4,15 @@ from expdj.apps.turk.views import (edit_hit, delete_hit, expire_hit,
 from expdj.apps.experiments.views import sync
 from django.views.generic.base import TemplateView
 from django.conf.urls import patterns, url
+from django.views.generic.base import TemplateView
+
+from expdj.apps.experiments.views import sync
+from expdj.apps.turk.api_views import ResultAPIList, BatteryResultAPIList
+from expdj.apps.turk.views import (
+    edit_hit, delete_hit, expire_hit, preview_hit, serve_hit, multiple_new_hit,
+    end_assignment, finished_view, not_consent_view, survey_submit, manage_hit
+)
+
 
 urlpatterns = patterns('',
     # HITS
@@ -43,5 +52,17 @@ urlpatterns = patterns('',
     url(r'^sync/(?P<rid>\d+|[A-Z]{8})/$',sync,name='sync_data'),
     url(r'^sync/$',sync,name='sync_data'),
     url(r'^finished$', finished_view, name="finished_view"),
+<<<<<<< e44430d1d3b46deb4c2e4ef6e37c175d82e2d8c9
     url(r'^worker/contact/(?P<aid>\d+)',contact_worker,name='contact_worker')
+=======
+
+    #  API
+    url(r'^api_/results/$', ResultAPIList.as_view(), name='result_api_list'),
+    url(
+        r'^api_/results/(?P<bid>\d+)/$',
+        BatteryResultAPIList.as_view(),
+        name='battery_result_api_list'
+    )
+
+>>>>>>> copied over old serializers from base url conf to new experiments and turk serializers files, added result list views to view all results owned by a user and all results for a given battery that are owned by a user
 )
