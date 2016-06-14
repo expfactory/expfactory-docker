d6ade380400f9155b99e0fd2c66d0136b2767d75
Ross Blair <rblair2@stanford.edu>
2016-06-14 13:34:43 -0700

    copied over old serializers from base url conf to new experiments and turk serializers files, added result list views to view all results owned by a user and all results for a given battery that are owned by a user

--- expdj/apps/turk/urls.py
+++ expdj/apps/turk/urls.py
@@ -1,9 +1,13 @@
-from expdj.apps.turk.views import edit_hit, delete_hit, expire_hit, preview_hit, \
-serve_hit, multiple_new_hit, end_assignment, finished_view, not_consent_view, \
-survey_submit, manage_hit
-from expdj.apps.experiments.views import sync
-from django.views.generic.base import TemplateView
 from django.conf.urls import patterns, url
+from django.views.generic.base import TemplateView
+
+from expdj.apps.experiments.views import sync
+from expdj.apps.turk.api_views import ResultAPIList, BatteryResultAPIList
+from expdj.apps.turk.views import (
+    edit_hit, delete_hit, expire_hit, preview_hit, serve_hit, multiple_new_hit,
+    end_assignment, finished_view, not_consent_view, survey_submit, manage_hit
+)
+
 
 urlpatterns = patterns('',
     # HITS
@@ -22,5 +26,14 @@ urlpatterns = patterns('',
     url(r'^surveys/(?P<rid>\d+|[A-Z]{8})/(?P<hid>[A-Za-z0-9]{30})/submit$',survey_submit,name='survey_submit'),
     url(r'^sync/(?P<rid>\d+|[A-Z]{8})/$',sync,name='sync_data'),
     url(r'^sync/$',sync,name='sync_data'),
-    url(r'^finished$', finished_view, name="finished_view")
+    url(r'^finished$', finished_view, name="finished_view"),
+
+    #  API
+    url(r'^api_/results/$', ResultAPIList.as_view(), name='result_api_list'),
+    url(
+        r'^api_/results/(?P<bid>\d+)/$',
+        BatteryResultAPIList.as_view(),
+        name='battery_result_api_list'
+    )
+
 )
