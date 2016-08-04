ef0be9f46eebd3a5239d88b24aa1504b7114fa47
Ross Blair <rosswilsonblair@gmail.com>
2016-08-04 04:10:01 -0400

    organizing by worker is inefficent for large amounts of results. Switched to use old serializers.

--- expdj/apps/turk/serializers.py
+++ expdj/apps/turk/serializers.py
@@ -3,22 +3,43 @@ from rest_framework import serializers
 from expdj.apps.experiments.serializers import (
     BatteryDescriptionSerializer, ExperimentTemplateSerializer
 )
+from expdj.apps.experiments.models import Battery, ExperimentTemplate, CognitiveAtlasTask
 from expdj.apps.turk.models import Result, Worker
 from expdj.apps.turk.utils import to_dict
 
+class BatterySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Battery
+        fields = ('name', 'description')
+
+class CognitiveAtlasTaskSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = CognitiveAtlasTask
+        fields = ('name','cog_atlas_id')
+
+class ExperimentTemplateSerializer(serializers.HyperlinkedModelSerializer):
+    cognitive_atlas_task = CognitiveAtlasTaskSerializer()
+    class Meta:
+        model = ExperimentTemplate
+        fields = ('exp_id','name','cognitive_atlas_task','reference','version','template')
+
+class WorkerSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Worker
+        fields = ["id"]
+
 class ResultSerializer(serializers.HyperlinkedModelSerializer):
     experiment = ExperimentTemplateSerializer()
+    battery = BatterySerializer()
+    worker = WorkerSerializer()
     data = serializers.SerializerMethodField('get_taskdata')
 
-    def get_taskdata(self, result):
+    def get_taskdata(self,result):
         return to_dict(result.taskdata)
 
     class Meta:
         model = Result
-        fields = [
-            'data', 'experiment', 'language', 'browser',
-            'platform', 'completed', 'finishtime'
-        ]
+        fields = ('data','experiment','battery','worker','language','browser','platform','completed','finishtime')
 
 class WorkerResultsSerializer(serializers.HyperlinkedModelSerializer):
     results = ResultSerializer(many=True, read_only=True, source='result_worker')
