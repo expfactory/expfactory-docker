23af029eb9174964eb433f35188b4f38507249a1
Ross Blair <rosswilsonblair@gmail.com>
2018-12-30 14:11:30 -0600

    properly return forbidden response in new api if requester not battery owner

--- expdj/apps/turk/api_views.py
+++ expdj/apps/turk/api_views.py
@@ -1,5 +1,5 @@
 from django.http.response import HttpResponseForbidden
-from rest_framework import generics, permissions, viewsets
+from rest_framework import exceptions, generics, permissions, viewsets
 
 from expdj.apps.experiments.models import Battery
 from expdj.apps.turk.models import Result, Worker
@@ -10,6 +10,6 @@ class BatteryResultAPIList(generics.ListAPIView):
 
     def get_queryset(self):
         battery_id = self.kwargs.get('bid')
-        if (Battery.objects.get(pk=battery_id).owner is not request.user):
-            raise HttpResponseForbidden()
+        if (Battery.objects.get(pk=battery_id).owner is not self.request.user.pk):
+            raise exceptions.PermissionDenied()
         return Result.objects.filter(battery__id=battery_id)
