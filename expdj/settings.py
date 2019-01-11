fc621ccb94d8b456ec50f5d182de06263b388d5c
Ross Blair <rosswilsonblair@gmail.com>
2019-01-11 14:31:57 -0800

    restframework needs pagination class explicitly set

--- expdj/settings.py
+++ expdj/settings.py
@@ -189,6 +189,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
 }
 
